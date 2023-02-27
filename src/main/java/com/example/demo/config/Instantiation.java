b7176e3c881af20e992d9e40ef6dc2e8c813f5df
Douglas Massote <douglasmpestana@gmail.com>
2023-02-27 16:55:36 -0300

    Post comments added

--- src/main/java/com/example/demo/config/Instantiation.java
+++ src/main/java/com/example/demo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
@@ -38,7 +39,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/02/2023"), "Good night", "Lorem ipsum 01", new AuthorDTO(alex));
 		Post post2 = new Post(null, sdf.parse("25/02/2023"), "Good morning", "Lorem ipsum 02", new AuthorDTO(alex));
-				
+		
+		CommentDTO c1 = new CommentDTO("Nice!", sdf.parse("26/02/2023"), new AuthorDTO(bob));
+		CommentDTO c2 = new CommentDTO("Not bad.", sdf.parse("27/02/2023"), new AuthorDTO(maria));
+		
+		post1.getComments().addAll(Arrays.asList(c1));
+		post2.getComments().addAll(Arrays.asList(c2));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		alex.getPosts().addAll(Arrays.asList(post1, post2));
